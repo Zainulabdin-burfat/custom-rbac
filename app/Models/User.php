1488cb5c03b49ec3cb4b30cdb475d96edb413c4a
Zain ul abdin <zainburfat112@gmail.com>
2022-12-12 13:42:43 +0500

    updates

--- app/Models/User.php
+++ app/Models/User.php
@@ -6,64 +6,16 @@ use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Foundation\Auth\User as Authenticatable;
 use Illuminate\Notifications\Notifiable;
 use Laravel\Passport\HasApiTokens;
+use App\Traits\UserPermissionTrait;
 
 class User extends Authenticatable
 {
-    use HasApiTokens, HasFactory, Notifiable;
+    use HasApiTokens, HasFactory, Notifiable, UserPermissionTrait;
 
     protected $fillable = ['name', 'email', 'password'];
 
     protected $hidden = ['remember_token', 'email_verified_at', 'password'];
 
-    public function roles()
-    {
-        return $this->belongsToMany(Role::class, 'role_user');
-    }
-
-    public function hasPermissionTo($permission)
-    {
-        foreach ($this->roles as $role) {
-            $permissions = $role->permissions->pluck('name')->toArray();
-            if (in_array($permission, $permissions))
-                return true;
-        }
-
-        return false;
-    }
-
-    public function permissions()
-    {
-        return $this->belongsToMany(Permission::class, 'permission_user');
-    }
-
-    public function hasDirectPermissionTo($permission)
-    {
-        foreach ($this->permissions->toArray() as $permissionName) {
-            if ($permission == $permissionName['name'])
-                return true;
-        }
-
-        return false;
-    }
-
-    public function hasRole($role)
-    {
-        foreach ($this->roles->toArray() as $roleName) {
-            if (strtolower($roleName['name']) == strtolower($role))
-                return true;
-        }
-        return false;
-    }
-
-    public function getRoleNames()
-    {
-        return $this->roles->pluck('name')->toArray();
-    }
-
-    public function getDirectPermissions()
-    {
-        return $this->permissions->pluck('name')->toArray();
-    }
 
     public function posts()
     {
